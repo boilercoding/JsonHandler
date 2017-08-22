29a8df79b25fa3b6d056117f09ee993a27ad8677
boilercoding <boilercoding@gmail.com>
2017-08-22 19:23:37 -0400

    Adds post method to redditapi interface

--- app/src/main/java/com/boilercoding/jsonhandler/RedditAPI.java
+++ app/src/main/java/com/boilercoding/jsonhandler/RedditAPI.java
@@ -1,9 +1,16 @@
 package com.boilercoding.jsonhandler;
 
+import java.util.Map;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import model.Feed;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Boilercoding on 8/22/2017.
@@ -16,4 +23,13 @@ public interface RedditAPI {
     @Headers("Content-Type: application/json")
     @GET(".json")
     Call<Feed> getData();
+
+    @POST("{user}")
+    Call<ResponseBody> login(
+            @HeaderMap Map<String, String> headers,
+            @Path("user") String username,  //codingwithmitch
+            @Query("user") String user,       //?user=codingwithmitch
+            @Query("passwd") String password, //&passwd=Mitchtabian1234!
+            @Query("api-type") String type    //&api-type=json
+    );
 }
